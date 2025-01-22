13fddde6164f17bf160e48d1a12147d9d4c6814f
Felipe Dev <felipedeveloper1k@gmail.com>
2025-01-22 16:03:17 +0100

    Condições terminadas e projeto feito de acordo com o pedido no CURSO

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let vitoriasDoJogador = partidas(35, 23)
+let vitoriasDoJogador = partidas(1234,1153)
 
 function partidas (v, d){
     let resultado = v - d
         if (resultado <= 10){
             console.log("Heroi tem saldo de " + resultado + " vitórias está no nivel FERRO")
         } else if (resultado <= 20){
-            console.log("Heroi tem saldo de " + resultado + " vitórias está no nivel Bronze")
+            console.log("Heroi tem saldo de " + resultado + " vitórias está no nivel BRONZE")
+        } else if(resultado <= 50){
+            console.log("Heroi tem saldo de " + resultado + " vitórias está no nivel PRATA")
+        } else if (resultado <= 80){
+            console.log("Heroi tem saldo de " + resultado + " vitórias está no nivel OURO")
+        } else if (resultado <= 90) {
+            console.log("Heroi tem saldo de " + resultado + " vitórias está no nivel DIAMANTE")
+        } else if (resultado <= 100){
+            console.log("Heroi tem saldo de " + resultado + " vitórias está no nivel LENDARIO")
+        }else {
+            console.log("Heroi tem saldo de " + resultado + " vitórias está no nivel IMORTAL")
         }
 }
